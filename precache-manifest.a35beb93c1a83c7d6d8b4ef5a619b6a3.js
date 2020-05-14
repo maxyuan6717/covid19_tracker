6e754dd81a913283333a96be06ef20007c030439
Max Yuan <maxyuan6717@gmail.com>
2020-05-13 22:20:13 -0400

    Updates

---
+++ precache-manifest.a35beb93c1a83c7d6d8b4ef5a619b6a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d48c17a3fb39480e24d8ec271a0e8f",
+    "url": "/covid19_tracker/index.html"
+  },
+  {
+    "revision": "555b6abb682ef687cdec",
+    "url": "/covid19_tracker/static/css/main.d241d0a9.chunk.css"
+  },
+  {
+    "revision": "812ad30530207cf10866",
+    "url": "/covid19_tracker/static/js/2.e74780a5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19_tracker/static/js/2.e74780a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "555b6abb682ef687cdec",
+    "url": "/covid19_tracker/static/js/main.ddf807a2.chunk.js"
+  },
+  {
+    "revision": "0efc2a12e51516cbe9d6",
+    "url": "/covid19_tracker/static/js/runtime-main.c70b5829.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19_tracker/static/media/image.d7265326.png"
+  }
+]);
